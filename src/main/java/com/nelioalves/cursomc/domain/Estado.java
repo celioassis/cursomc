d6086ee879a059218d1f07a5606c4e5dae1473e0
Célio A. Assis <celioassis@gmail.com>
2018-06-24 19:23:00 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/nelioalves/cursomc/domain/Estado.java
+++ src/main/java/com/nelioalves/cursomc/domain/Estado.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 
@@ -20,8 +22,9 @@ public class Estado implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="estado")
-	private List<Cidade> Cidades = new ArrayList<>();
+	private List<Cidade> cidades = new ArrayList<>();
 	
 	public Estado() {
 		
@@ -50,11 +53,11 @@ public class Estado implements Serializable {
 	}
 
 	public List<Cidade> getCidades() {
-		return Cidades;
+		return cidades;
 	}
 
 	public void setCidades(List<Cidade> cidades) {
-		Cidades = cidades;
+		this.cidades = cidades;
 	}
 
 	@Override
